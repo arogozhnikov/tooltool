ab7194fc23db0e5fc700d2796acd9f504cb08ea5
Alex Rogozhnikov <iamfullofspam@gmail.com>
2023-09-30 22:19:00 -0700

    add timer to utilities

---
+++ tooltool/timer.py
@@ -0,0 +1,13 @@
+import time
+
+class Timer:
+    def __init__(self, name):
+        self.name = name
+        self.start_time = time.time()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *_args):
+        self.time_taken = time.time() - self.start_time
+        print(f"Spent {self.time_taken:.3f} seconds in timer {self.name}")
